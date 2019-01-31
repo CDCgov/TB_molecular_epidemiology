7b785c74a76008c575141fe3edf8f26a0c7bbaf6
Winglee <nrf1@cdc.gov>
2019-01-31 10:36:04 -0500

    Improved layout

--- LITT/littGimsShiny.R
+++ LITT/littGimsShiny.R
@@ -23,72 +23,76 @@ ui <- fluidPage(
     column(3,
            textInput("prefix", "Name prefix for output files"),
            sliderInput("snpCutoff", "SNP cutoff", min=0, max=10, value=5, step=1, round=T),
-           checkboxInput("incDateTab", "Check if to output dates used in IP calculations"),
+           checkboxInput("incDateTab", "Check to output dates used in IP calculations"),
            br(),
            h3("Included cases"),
-           helpText("Upload or type state case numbers to analyze. ",
-                    "This is optional and if not provided, LITT will analyze all cases in the inputs."),
+           # helpText("Upload or type state case numbers to analyze. ",
+           #          "This is optional and if not provided, LITT will analyze all cases in the inputs."),
            fileInput("caseListFile", "Upload list of state case numbers to analyze"),
            textAreaInput("caseListManual", "Type list of state case numbers", rows=5)),
     column(3,
-           # h3("Input files"),
-           fileInput("distMatrix", "SNP distance matrix"),
-           checkboxInput("BNdist",
-                         "Check if this contains accession numbers that must be converted to state case number", value=F),
-           br(),
-           fileInput("epi", "Epi link list")),
-           fileInput("caseData", "Case data"),
-           # br(),
-           # br(),
-           # actionButton("clear", "Clear inputs"),
-           # br(),
-           # br(),
-           # actionButton("run", "Run", style='background-color:royalblue; color:white; padding:20px 40px')),
+           # fluidRow(
+             # h3("Input files"),
+             fileInput("distMatrix", "SNP distance matrix"),
+             checkboxInput("BNdist",
+                           "Check if this contains accession numbers that must be converted to state case number", value=F),
+             br(),
+             fileInput("epi", "Epi link list"),
+             fileInput("caseData", "Case data")),
+           # fluidRow(align="center",
+           #          br(),
+           #          br(),
+           #          br(),
+           #          br(),
+           #          br(),
+           #          br(),
+           #          actionButton("clear", "Clear inputs"))),
+    # br(),
+    # br(),
+    # actionButton("run", "Run", style='background-color:royalblue; color:white; padding:20px 40px')),
     # helpText("Case data includes, if available, columns for symptom onset, ",
     #          "infectious period start and end, and risk factors."),
     column(1),
     column(width=5, #https://stackoverflow.com/questions/37472915/r-shiny-how-to-generate-this-layout-with-nested-rows-in-column-2
            fluidRow(
              column(4, align="center",
-                    lapply(gimsVars[1:7], function(g) {
+                    lapply(gimsVars[1:5], function(g) {
                       numericInput(g, g, 0, min = 0, step = 1, width="70px")
                       # sliderInput(g, g, min=0, max=3, value=0, step=1, round=T)
                     })),
              column(4, align="center", 
-                    # br(),
-                    lapply(gimsVars[8:14], function(g) {
+                    lapply(gimsVars[6:10], function(g) {
                       numericInput(g, g, 0, min = 1, step = 1, width="70px")
                     })),
-             column(4, 
-                    helpText("Weight > 0 means RF included in ranking. Weight = 0 means RF not in ranking but in outputs. Weight < 0 means RF not in ranking or outputs.", 
-                             align="left"))),
+             column(4, align="center", 
+                    lapply(gimsVars[11:14], function(g) {
+                      numericInput(g, g, 0, min = 1, step = 1, width="70px")
+                    }))),
+           # helpText("Weight > 0 means RF included in ranking. Weight = 0 means RF not in ranking but in outputs. Weight < 0 means RF not in ranking or outputs.", 
+           #          align="left"))),
            fluidRow(
              column(12, 
-                    # helpText("Weight > 0 means RF included in ranking. Weight = 0 means RF not in ranking but in outputs. Weight < 0 means RF not in ranking or outputs.", 
-                    #          align="left"),
-                    br(),
+                    helpText("Weight > 0 means RF included in ranking. Weight = 0 means RF not in ranking but in outputs. Weight < 0 means RF not in ranking or outputs.",
+                             align="left"),
+                    # br(),
                     h4("Additional Risk Factors", align="center"),
                     fileInput("rfTable", "Table of risk factor weights", accept=c(".xlsx", ".csv")))))),
-                    # helpText("This table contains a list of the columns in the case data table to use a risk factors, with their weights.",
-                    #          "Variable names must exactly match the name of the column in the case data table."))))),
-    fluidRow(),
-    fluidRow(column(12, align="center",
-                    actionButton("clear", "Clear inputs"),
-                    br(),
-                    br(),
-                    actionButton("run", "Run", style='background-color:royalblue; color:white; padding:20px 40px'))), #https://www.w3schools.com/css/css3_buttons.asp
-    fluidRow(column(12, align="center",
-                    br(),
-                    br(),
-                    htmlOutput("message"))),
-    fluidRow(column(12, align="center",
-                    br(),
-                    downloadButton("downloadData", "Download Results")))
-  )
+  # helpText("This table contains a list of the columns in the case data table to use a risk factors, with their weights.",
+  #          "Variable names must exactly match the name of the column in the case data table."))))),
+  # fluidRow(),
+  fluidRow(column(12, align="center",
+                  actionButton("clear", "Clear inputs"),
+                  actionButton("run", "Run", style='background-color:royalblue; color:white; padding:20px 40px'), #https://www.w3schools.com/css/css3_buttons.asp
+                  downloadButton("downloadData", "Download Results"),                
+                  br(),
+                  br(),
+                  htmlOutput("message")))
+)
 
 # Define server logic ----
 server <- function(input, output, session) {
   output$message <- renderText({"test"})
+  updateTextInput(session,'caseListManual',value="test") 
 }
 
 # Run the app ----
