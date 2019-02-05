36baa8d3fc865491a92c8b2d1b9a2ea8173dc1f2
Winglee <NRF1@cdc.gov>
2019-02-05 17:02:26 -0500

    List of cases to include in analysis, either via inputs or uploaded list, with memory of deleted cases in list

--- LITT/littGimsShiny.R
+++ LITT/littGimsShiny.R
@@ -28,12 +28,12 @@ ui <- fluidPage(
            checkboxInput("writeDate", "Output dates used in IP calculations"),
            br(),
            h3("Included cases"),
-           fileInput("caseListFile", "Upload list of state case numbers to analyze"),
+           fileInput("caseListFile", "Upload list of state case numbers to analyze", accept=c(".xlsx", ".csv")),
            textAreaInput("caseListManual", "Type list of state case numbers", rows=5)),
     column(3,
-           fileInput("caseData", "Case data"),
-           fileInput("epi", "Epi link list"),
-           fileInput("distMatrix", "SNP distance matrix"),
+           fileInput("caseData", "Case data", accept=c(".xlsx", ".csv")),
+           fileInput("epi", "Epi link list", accept=c(".xlsx", ".csv")),
+           fileInput("distMatrix", "SNP distance matrix", accept=c(".xlsx", ".csv", ".txt")),
            checkboxInput("writeDist",
                          "Output distance matrix", value=F)),
     column(1),
@@ -62,6 +62,18 @@ ui <- fluidPage(
                   htmlOutput("message")))
 )
 
+##text to format html string to increase message font size
+outputfontsizestart = "<font font-size=\"30px\"><b>"
+outputfontsizeend = "</b></font>"
+
+##set up for downloading results
+dash <- .Platform$file.sep #file separator
+tmpdir = tempdir() #temporary directory to write outputs
+tmpdir = paste(tmpdir, dash, sep="")
+outfiles = NA #list of output files
+incCases = "" #list of cases to include in analysis
+delCases = "MRCA" #list of cases that were deleted by the user in the manual text field
+
 # Define server logic ----
 server <- function(input, output, session) {
   shinyjs::hide("downloadData")
@@ -70,75 +82,94 @@ server <- function(input, output, session) {
                        clEpi = F,
                        clDist = F,
                        clRF = F,
-                       clCaseList = F) #variables that if true, indicate that clear has been hit but a new table has not been uploaded
+                       clCaseList = F, #variables that if true, indicate that clear has been hit but a new table has not been uploaded
+                       # incCases = "", #list of cases to include in analysis
+                       # delCases = "", #list of cases that were deleted by the user in the manual text field
+                       inputCaseList = F, #if true, case list was input, so don't update list when other files are uploaded
+                       caseData = NA, #add the caseData, dist, and epi in case they get read to get case list so don't read twice
+                       dist = NA,
+                       epi = NA)
   
   ##run LATTE when action button hit
   observeEvent(input$run, {
     ##case data
-    if(rv$clCaseData) {
-      caseData = NA
-    } else {
-      caseData = readShinyInputFile(input$caseData)
-    }
-    if(all(is.na(caseData)) | rv$clCaseData) {
-      output$message <- renderText({paste(outputfontsizestart, "No case data. Please input a case data table.", outputfontsizeend, sep="")})
-      return(NULL)
-    }
-    ##set up progress and output prefix
-    progress <- Progress$new(session, min=-2, max=13)
-    on.exit(progress$close())
-    progress$set(message = "Running LITT")
-    output$message <- renderText({paste(outputfontsizestart, "Analyzing data", outputfontsizeend, sep="")})
-    progress$set(value=0)
-    outPrefix = paste(tmpdir, input$prefix, sep="")
-    ##distance matrix, epi and rf table
-    if(rv$clDist) {
-      dist = NA
-    } else {
-      dist = readShinyDistanceMatrix(input$distMatrix, bn=F, log = paste(outPrefix, defaultLogName, sep=""))
-    }
-    if(rv$clEpi) {
-      epi = NA
-    } else {
-      epi = readShinyInputFile(input$epi)
-    }
-    if(rv$clRF) {
-      rf = NA
-    } else {
-      rf = readShinyInputFile(input$rfTable)
-    }
-    ##gims risk factors
-    gimsRF = data.frame(variable = gimsVars,
-                         weight = sapply(1:length(gimsVars), function(i){input[[gimsVars[i]]]}))
-    gimsRF = gimsRF[gimsRF$weight >= 0,]
-    if(nrow(gimsRF) < 1) {
-      gimsRF = NA
-    }
-    print(gimsRF)
+    # if(rv$clCaseData) {
+    #   caseData = NA
+    # } else if(all(is.na(rv$caseData))) {
+    #   caseData = readShinyInputFile(input$caseData)
+    # } else {
+    #   caseData = rv$caseData
+    # }
+    # if(all(is.na(caseData)) | rv$clCaseData) {
+    #   output$message <- renderText({paste(outputfontsizestart, "No case data. Please input a case data table.", outputfontsizeend, sep="")})
+    #   return(NULL)
+    # }
+    # ##set up progress and output prefix
+    # progress <- Progress$new(session, min=-2, max=13)
+    # on.exit(progress$close())
+    # progress$set(message = "Running LITT")
+    # output$message <- renderText({paste(outputfontsizestart, "Analyzing data", outputfontsizeend, sep="")})
+    # progress$set(value=0)
+    # outPrefix = paste(tmpdir, input$prefix, sep="")
+    # ##distance matrix, epi and rf table
+    # if(rv$clDist) {
+    #   dist = NA
+    # } else if(all(is.na(rv$dist))){
+    #   dist = readShinyDistanceMatrix(input$distMatrix, bn=F, log = paste(outPrefix, defaultLogName, sep=""))
+    # } else {
+    #   dist = rv$dist
+    # }
+    # if(rv$clEpi) {
+    #   epi = NA
+    # } else if(all(is.na(rv$epi))){
+    #   epi = readShinyInputFile(input$epi)
+    # } else {
+    #   epi = rv$epi
+    # }
+    # if(rv$clRF) {
+    #   rf = NA
+    # } else {
+    #   rf = readShinyInputFile(input$rfTable)
+    # }
+    # ##gims risk factors
+    # gimsRF = data.frame(variable = gimsVars,
+    #                     weight = sapply(1:length(gimsVars), function(i){input[[gimsVars[i]]]}))
+    # gimsRF = gimsRF[gimsRF$weight >= 0,]
+    # if(nrow(gimsRF) < 1) {
+    #   gimsRF = NA
+    # }
     
-    res = tryCatch({
-      littres = littGims(outPrefix = outPrefix,
-                         caseData = caseData,
-                         dist = dist,
-                         epi = epi,
-                         SNPcutoff = input$snpCutoff,
-                         rfTable = rf,
-                         gimsRiskFactor = gimsRF,
-                         cdFromGimsRun = input$cdFromGimsRun,
-                         writeDate = input$writeDate,
-                         writeDist = input$writeDist,
-                         appendlog = F,
-                         progress = progress)
-      outfiles <<- littres$outputFiles
-      output$message <- renderText({paste(outputfontsizestart, "Analysis complete", outputfontsizeend, sep="")})
-      shinyjs::show("downloadData")
-    }, error = function(e) {
-      outfiles <<- paste(tmpdir, input$prefix, defaultLogName, sep="")
-      output$message <- renderText({paste(outputfontsizestart, "Error detected:<br/>", geterrmessage(),
-                                          "<br/>Download and view log for more details.", outputfontsizeend, sep="")})
-      cat(geterrmessage(), file = outfiles, append = T)
-      shinyjs::show("downloadData")
-    })
+    ##case list
+    cases = NA
+    if(input$caseListManual != "") {
+      cases = strsplit(input$caseListManual, "\n")[[1]]
+    }
+    print(cases)
+    print(delCases)
+    # res = tryCatch({
+    #   littres = littGims(outPrefix = outPrefix,
+    #                      cases = cases,
+    #                      caseData = caseData,
+    #                      dist = dist,
+    #                      epi = epi,
+    #                      SNPcutoff = input$snpCutoff,
+    #                      rfTable = rf,
+    #                      gimsRiskFactor = gimsRF,
+    #                      cdFromGimsRun = input$cdFromGimsRun,
+    #                      writeDate = input$writeDate,
+    #                      writeDist = input$writeDist,
+    #                      appendlog = F,
+    #                      progress = progress)
+    #   outfiles <<- littres$outputFiles
+    #   output$message <- renderText({paste(outputfontsizestart, "Analysis complete", outputfontsizeend, sep="")})
+    #   shinyjs::show("downloadData")
+    # }, error = function(e) {
+    #   outfiles <<- paste(tmpdir, input$prefix, defaultLogName, sep="")
+    #   output$message <- renderText({paste(outputfontsizestart, "Error detected:<br/>", geterrmessage(),
+    #                                       "<br/>Download and view log for more details.", outputfontsizeend, sep="")})
+    #   cat(geterrmessage(), file = outfiles, append = T)
+    #   shinyjs::show("downloadData")
+    # })
   })
   
   ##zip and download outputs
@@ -177,9 +208,12 @@ server <- function(input, output, session) {
     rv$clDist <- T
     rv$clRF <- T
     rv$clCaseList <- T
-    updateCheckboxInput(session, "writeDist", value=T)
+    incCases <<- ""
+    delCases <<- "MRCA"
+    rv$inputCaseList <- F
+    updateCheckboxInput(session, "writeDist", value=F)
     updateCheckboxInput(session, "writeDate", value=F)
-    updateTextInput(session,'caseListManual',value="") 
+    updateTextInput(session,"caseListManual",value="") 
   })
   
   ##if any inputs change, hide download button and remove output message
@@ -200,18 +234,50 @@ server <- function(input, output, session) {
   })
   observe({
     input$caseData
+    if(!is.null(input$caseData) & !rv$inputCaseList & !rv$clCaseData) {
+      rv$caseData = fixStcasenoName(readShinyInputFile(input$caseData))
+      incases = as.character(rv$caseData$STCASENO)
+      if(all(incCases=="")) {
+        incCases <<- sort(unique(incases))
+      } else {
+        incCases <<- sort(unique(c(incCases[incCases!=""], incases)))
+      }
+      incCases <<- incCases[!incCases %in% delCases] #remove previously deleted cases
+      updateTextInput(session, "caseListManual", value=paste(incCases, collapse="\n"))
+    }
     rv$clCaseData <- F
     output$message <- renderText({""})
     shinyjs::hide("downloadData")
   })
   observe({
     input$epi
+    if(!is.null(input$epi) & !rv$inputCaseList & !rv$clEpi) {
+      rv$epi = fixEpiNames(fixStcasenoName(readShinyInputFile(input$epi)), log = paste(tmpdir, input$prefix, defaultLogName, sep=""))
+      if(all(incCases=="")) {
+        incCases <<- sort(unique(as.character(c(rv$epi$case1, rv$epi$case2))))
+      } else {
+        incCases <<- sort(unique(as.character(c(incCases[incCases!=""], rv$epi$case1, rv$epi$case2))))
+      }
+      incCases <<- incCases[!incCases %in% delCases] #remove previously deleted cases
+      updateTextInput(session, "caseListManual", value=paste(incCases, collapse="\n"))
+    }
     rv$clEpi <- F
     output$message <- renderText({""})
     shinyjs::hide("downloadData")
   })
   observe({
     input$distMatrix
+    if(!is.null(input$distMatrix) & !rv$inputCaseList & !rv$clDist) {
+      rv$dist = fixStcasenoName(readShinyDistanceMatrix(input$distMatrix, bn=F, 
+                                                        log = paste(tmpdir, input$prefix, defaultLogName, sep="")))
+      if(all(incCases=="")) {
+        incCases <<- sort(unique(row.names(rv$dist)))
+      } else {
+        incCases <<- sort(unique(c(incCases[incCases!=""], row.names(rv$dist))))
+      }
+      incCases <<- incCases[!incCases %in% delCases] #remove previously deleted cases
+      updateTextInput(session, "caseListManual", value=paste(incCases, collapse="\n"))
+    }
     rv$clDist <- F
     output$message <- renderText({""})
     shinyjs::hide("downloadData")
@@ -229,12 +295,23 @@ server <- function(input, output, session) {
   })
   observe({
     input$caseListFile
+    if(!is.null(input$caseListFile) & !rv$clCaseList) {
+      rv$inputCaseList = T
+      res = fixStcasenoName(readShinyInputFile(input$caseListFile))
+      incCases <<- res$STCASENO
+      updateTextInput(session, "caseListManual", value=paste(incCases, collapse="\n"))
+    }
     rv$clCaseList = F
     output$message <- renderText({""})
     shinyjs::hide("downloadData")
   })
   observe({
     input$caseListManual
+    if(input$caseListManual != "") {
+      rvcases = incCases
+      mancases = strsplit(input$caseListManual, "\n")[[1]]
+      delCases <<- unique(c(delCases, rvcases[!rvcases %in% mancases])) #list of cases deleted by user, to not include with future uploads
+    }
     output$message <- renderText({""})
     shinyjs::hide("downloadData")
   })
