cc8cb5ce16397e51efc07d31399a5e280ac1d367
Winglee <NRF1@cdc.gov>
2020-02-13 09:35:22 -0500

    add no time tab to interface

--- LATTE/app.R
+++ LATTE/app.R
@@ -9,47 +9,95 @@ source("latte.R")
 # Define UI ----
 ui <- fluidPage(
   # titlePanel("LATTE"),
-  fluidRow(column(10, offset=2, titlePanel("LATTE"))),
-  # column(12, align="center", titlePanel("LATTE")),
+  # fluidRow(column(10, offset=2, titlePanel("LATTE"))),
+  fluidRow(column(12, align="center", titlePanel("LATTE"))),
   useShinyjs(),
-  fluidRow(
-    column(2),
-    column(4,
-           h3("Set up inputs"),
-           p("Warning: do not upload personally identifiable information (PII)", style="color:red"),
-           fileInput("locTab", "Table of dates in locations (required)", accept=c(".xlsx", ".csv")),
-           fileInput("ipTab", "Table of infectious periods", accept=c(".xlsx", ".csv")),
-           br(),
-           br(),
-           h3("Set up outputs"),
-           textInput("prefix", "Name prefix for output files")),
-    column(4,
-           h3("Set up link definitions"),
-           radioButtons("linkType", "Type of link",
-                        choiceNames = list("Epi link", "IP epi link"),
-                        choiceValues = list("epi", "ipepi"),
-                        selected = "epi"),
-           sliderInput("epicutoff", "Number of days cases must overlap in a location to form a definite or probable epi link", min=0, max=30, value=defaultCut, step=1, round=T),
-           sliderInput("ipepicutoff", "Number of days cases must overlap each other and an IP to form an IP epi link", min=0, max=30, value=defaultCut, step=1, round=T),
-           checkboxInput("removeAfter", "Include overlaps that occur after either IP end as potential IP epi links (e.g. to identify potential re-exposure during a contact investigation)")),
-           # actionButton("clear", "Clear inputs")),
-    column(2)),
+  tabsetPanel(
+    tabPanel("With Time Data",
+             # fluidRow(column(1),
+             #          column(11,
+             #                 br(),
+             #          p("Identify overlaps in time and location"))),
+             fluidRow(br(),
+                      h4("Identify overlaps in time and location", align="center")),
+             fluidRow(
+               column(2),
+               column(4,
+                      h3("Set up inputs"),
+                      p("Warning: do not upload personally identifiable information (PII)", style="color:red"),
+                      fileInput("locTab", "Table of dates in locations (required)", accept=c(".xlsx", ".csv")),
+                      fileInput("ipTab", "Table of infectious periods", accept=c(".xlsx", ".csv")),
+                      br(),
+                      br(),
+                      h3("Set up outputs"),
+                      textInput("prefix", "Name prefix for output files")),
+               column(4,
+                      h3("Set up link definitions"),
+                      radioButtons("linkType", "Type of link",
+                                   choiceNames = list("Epi link", "IP epi link"),
+                                   choiceValues = list("epi", "ipepi"),
+                                   selected = "epi"),
+                      sliderInput("epicutoff", "Overlap threshold (number of days people must overlap in a location to form a definite or probable epi link)", min=0, max=30, value=defaultCut, step=1, round=T),
+                      sliderInput("ipepicutoff", "Overlap threshold (number of days people must overlap each other and an IP to form an IP epi link)", min=0, max=30, value=defaultCut, step=1, round=T),
+                      checkboxInput("removeAfter", "Include re-infection (overlaps that occur after either IP end as potential IP epi links, e.g. to identify potential re-exposure during a contact investigation)")),
+               # actionButton("clear", "Clear inputs")),
+               column(2)),
+             
+             fluidRow(column(12, align="center",
+                             br(),
+                             actionButton("clear", "Clear inputs"),
+                             br(),
+                             br(),
+                             actionButton("run", "Run", style='background-color:royalblue; color:white; padding:20px 40px'))), #https://www.w3schools.com/css/css3_buttons.asp
+             fluidRow(column(12, align="center",
+                             br(),
+                             br(),
+                             htmlOutput("message"))), #use instead of textOutput so can change font of returning string: https://stackoverflow.com/questions/24049159/change-the-color-and-font-of-text-in-shiny-app
+             fluidRow(column(12, align="center",
+                             br(),
+                             downloadButton("downloadData", "Download Results"))
+                      
+             )),
+             # ))))
+    tabPanel("Without Time Data",
+             fluidRow(br(),
+                      h4("Identify all possible pairs from lists of people", align="center")),
+             fluidRow(
+               column(2),
+               column(4,
+                      h3("Set up inputs"),
+                      p("Warning: do not upload personally identifiable information (PII)", style="color:red"),
+                      fileInput("noTimeTab", "Table of people in locations (required)", accept=c(".xlsx", ".csv")),
+                      br(),
+                      br(),
+                      h3("Set up outputs"),
+                      textInput("noTimePrefix", "Name prefix for output files")),
+               column(4,
+                      h3("Set up link definitions"),
+                      radioButtons("noTimeStrength", "Strength of link",
+                                   choiceNames = list("Definite epi link", "Probable epi link", "Possible epi link",
+                                                      "No strength", "Custom strength"),
+                                   choiceValues = list("definite", "probable", "possible", "none", "custom"),
+                                   selected = "probable"),
+                      column(2),
+                      column(10,
+                             fileInput("noTimeCustomStrength", "Custom strength table", accept=c(".xlsx", ".csv")))),
+               column(2)),
 
-    fluidRow(column(12, align="center",
-                    br(),
-                    actionButton("clear", "Clear inputs"),
-                    br(),
-                    br(),
-                    actionButton("run", "Run", style='background-color:royalblue; color:white; padding:20px 40px'))), #https://www.w3schools.com/css/css3_buttons.asp
-    fluidRow(column(12, align="center",
-                    br(),
-                    br(),
-                    htmlOutput("message"))), #use instead of textOutput so can change font of returning string: https://stackoverflow.com/questions/24049159/change-the-color-and-font-of-text-in-shiny-app
-    fluidRow(column(12, align="center",
-                    br(),
-                    downloadButton("downloadData", "Download Results"))
+             fluidRow(column(12, align="center",
+                             br(),
+                             actionButton("noTimeClear", "Clear inputs"),
+                             br(),
+                             br(),
+                             actionButton("noTimeRun", "Run", style='background-color:royalblue; color:white; padding:20px 40px'))), #https://www.w3schools.com/css/css3_buttons.asp
+             fluidRow(column(12, align="center",
+                             br(),
+                             br(),
+                             htmlOutput("noTimeMessage"))), #use instead of textOutput so can change font of returning string: https://stackoverflow.com/questions/24049159/change-the-color-and-font-of-text-in-shiny-app
+             fluidRow(column(12, align="center",
+                             br(),
+                             downloadButton("noTimeDownloadData", "Download Results"))))))
 
-    ))
 
 ##text to format html string to increase message font size
 outputfontsizestart = "<font font-size=\"30px\"><b>"
@@ -64,10 +112,12 @@ outfiles = NA #list of output files
 # Define server logic ----
 server <- function(input, output, session) {
   shinyjs::hide("downloadData")
-
+  
   rv <- reactiveValues(clLoc = F,
-                       clIP = F) #variables that if true, indicate that clear has been hit but a new table has not been uploaded
-
+                       clIP = F,
+                       clNTtab = F,
+                       clNTcust = F) #variables that if true, indicate that clear has been hit but a new table has not been uploaded
+  
   ##run LATTE when action button hit
   observeEvent(input$run, {
     if(rv$clLoc) {
@@ -111,7 +161,7 @@ server <- function(input, output, session) {
       shinyjs::show("downloadData")
     })
   })
-
+  
   ##zip and download outputs
   output$downloadData <- downloadHandler(
     filename = function() {
@@ -130,62 +180,134 @@ server <- function(input, output, session) {
     },
     contentType = "application/zip"
   )
-
+  
   ##clear inputs if clear button is clicked
   observeEvent(input$clear, {
     updateTextInput(session, "prefix", value="")
+    updateTextInput(session, "noTimePrefix", value="")
     updateSliderInput(session, "epicutoff", value=defaultCut)
     updateSliderInput(session, "ipepicutoff", value=defaultCut)
     updateCheckboxInput(session, "removeAfter", value=F)
     # updateRadioButtons(session, "linkType", selected = epi)
     reset("linkType")
     output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
     shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
     reset("locTab")
     reset("ipTab")
+    reset("noTimeTab")
+    reset("noTimeCustomStrength")
     rv$clLoc <- T
     rv$clIP <- T
+    rv$clNTtab <- T
+    rv$clNTcust <- T
   })
-
+  observeEvent(input$noTimeClear, {
+    updateTextInput(session, "prefix", value="")
+    updateTextInput(session, "noTimePrefix", value="")
+    updateSliderInput(session, "epicutoff", value=defaultCut)
+    updateSliderInput(session, "ipepicutoff", value=defaultCut)
+    updateCheckboxInput(session, "removeAfter", value=F)
+    reset("linkType")
+    reset("noTimeStrength")
+    output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
+    shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
+    reset("locTab")
+    reset("ipTab")
+    reset("noTimeTab")
+    reset("noTimeCustomStrength")
+    rv$clLoc <- T
+    rv$clIP <- T
+    rv$clNTtab <- T
+    rv$clNTcust <- T
+  })
+  
   ##if any inputs change, hide download button and remove output message
   observe({
     input$locTab
     rv$clLoc <- F
     output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
     shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
   })
   observe({
     input$ipTab
     rv$clIP <- F
     output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
     shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
   })
   observe({
     input$prefix
     output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
     shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
   })
   observe({
     input$linkType
     output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
     shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
   })
   observe({
     input$epicutoff
     output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
     shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
   })
   observe({
     input$ipepicutoff
     output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
     shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
   })
   observe({
     input$removeAfter
     output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
     shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
   })
-
+  observe({
+    input$noTimeTab
+    rv$clNTtab <- F
+    output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
+    shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
+  })
+  observe({
+    input$noTimePrefix
+    output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
+    shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
+  })
+  observe({
+    input$noTimeStrength
+    output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
+    shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
+  })
+  observe({
+    input$noTimeCustomStrength
+    rv$clNTcust <- F
+    output$message <- renderText({""})
+    output$noTimeMessage <- renderText({""})
+    shinyjs::hide("downloadData")
+    shinyjs::hide("noTimeDownloadData")
+  })
+  
   ##show correct cutoff and check box, depending on link type
   observe({
     if(input$linkType == "ipepi") {
@@ -196,6 +318,15 @@ server <- function(input, output, session) {
       shinyjs::show("epicutoff")
       shinyjs::hide("ipepicutoff")
       shinyjs::hide("removeAfter")
+    }
+  })
+  
+  ##show custom strength upload only when selected
+  observe({
+    if(input$noTimeStrength == "custom") {
+      shinyjs::show("noTimeCustomStrength")
+    } else {
+      shinyjs::hide("noTimeCustomStrength")
     }
   })
 }
